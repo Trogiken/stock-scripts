2e0a02e0a91686c03a43e560e6a886c939167acf
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-11-02 19:48:34 -0400

    More data points add, custom save location

--- Report-Analyzer/analyzer.py
+++ Report-Analyzer/analyzer.py
@@ -31,22 +31,22 @@ except ImportError:
     import_error = True
     print("Please install shutil package: pip install shutil")
 
-
 if import_error:
     input("\nPress ENTER to exit...")
     sys.exit()
 
 
-
 # TODO add gui window to select file, date range, and output file location/name
 # TODO if no date range, sort by months of data into separate tables
 # TODO store output data in a separate directory
+# TODO Check if TK operations work on Mac and Linux, if not using Qt6
 
-def analyze_data(account_history_path, history):
-    """Analyze the data from the CSV files and return the dataframes with the results"""
+# Original Short Sale Price = Profit Price + (Profit Price - (After Balance - Before Balance) / Quantity)
+
+def analyze_data(account_history_path):
+    """Analyze the data from the CSV file and return the dataframe with the results"""
     # Read the CSV file
     acc_df = pd.read_csv(account_history_path, sep=',')
-    history_df = pd.read_csv(account_history_path, sep=',')
 
     # remove whitespace from balance columns while keeping it a float
     acc_df['Balance Before'] = acc_df['Balance Before'].str.replace('\xa0', '').astype(float)
@@ -54,28 +54,35 @@ def analyze_data(account_history_path, history):
 
     # regex patterns to extract the data we need
     symbol_pattern = r"symbol (\w+:\w+)"
-    price_pattern = r"price (\d+\.\d+)"
+    closed_price_pattern = r"price (\d+\.\d+)"
     shares_pattern = r"for (\d+) shares"
     position_type_pattern = r"Close (long|short) position"
 
     # Apply the regex patterns to each element of the 'Action' column
-    position = acc_df['Action'].apply(lambda x: re.search(position_type_pattern, x).group(1))
-    symbol = acc_df['Action'].apply(lambda x: re.search(symbol_pattern, x).group(1))
-    quantity = acc_df['Action'].apply(lambda x: re.search(shares_pattern, x).group(1))
-    price = acc_df['Action'].apply(lambda x: re.search(price_pattern, x).group(1))
+    details_list = []
+    for _, row in acc_df.iterrows():
+        if "Commission" in row['Action']:  # TODO add commission to P&L, and make it a separate column
+            continue
+        position = re.search(position_type_pattern, row['Action']).group(1)  # long or short position
+        symbol = re.search(symbol_pattern, row['Action']).group(1)  # symbol of the stock
+        quantity = re.search(shares_pattern, row['Action']).group(1)  # quantity of shares
+        opened_price = float(re.search(closed_price_pattern, row['Action']).group(1)) + ((row['Balance After'] - row['Balance Before']) / int(quantity))  # price at which the position was opened
+        closed_price = re.search(closed_price_pattern, row['Action']).group(1)  # price at which the position was closed
+        details_list.append({
+            'Time': row['Time'],
+            'Position': position,
+            'Symbol': symbol,
+            'Quantity': quantity,
+            'Opened Price': opened_price,
+            'Closed Price': closed_price,
+            'Balance Before': row['Balance Before'],
+            'Balance After': row['Balance After'],
+            'P&L': row['Balance After'] - row['Balance Before'],
+            '%': (row['Balance After'] - row['Balance Before']) / row['Balance Before'] * 100,
+        })
 
     # Create a new DataFrame with only the columns we need
-    details_df = pd.DataFrame({
-        'Time': acc_df['Time'],
-        'Position': position,
-        'Symbol': symbol,
-        'Quantity': quantity,
-        'Take Profit Price': price,  # TODO This is not the price at which the position was opened, add that one too. Use the close time in the history csv file to match with this data.
-        'Balance Before': acc_df['Balance Before'],
-        'Balance After': acc_df['Balance After'],
-        'P&L': acc_df['Balance After'] - acc_df['Balance Before'],
-        '%': (acc_df['Balance After'] - acc_df['Balance Before']) / acc_df['Balance Before'] * 100,
-    })
+    details_df = pd.DataFrame(details_list)
 
     # total_return_amount = [details_df['P&L'].sum()]
     total_return_percentage = [details_df['%'].sum()]
@@ -101,20 +108,15 @@ def analyze_data(account_history_path, history):
     # plt.bar(labels, sizes, color=['green', 'green', 'green', 'green', 'red', 'green'])
     # plt.savefig(os.path.join(str(Path(__file__).resolve()), 'output', 'pie_chart.png'))
 
-    return details_df, total_df 
+    return details_df, total_df
 
 
-def export_html(details_df, total_df):
+def export_html(details_df, total_df, export_location):
     """Export the DataFrame to an HTML file"""
-    output_directory = os.path.join(str(Path(__file__).resolve().parents[0]), 'output')
-    export_file_location = os.path.join(output_directory, 'report.html')
-
-    if os.path.exists(output_directory):
-        # remove dir even if files are inside
-        shutil.rmtree(output_directory)
-    os.makedirs(output_directory)
+    if os.path.exists(export_location):
+        os.remove(export_location)
     
-    with open(export_file_location, 'w') as f:
+    with open(export_location, 'w') as f:
         f.write('<html>\n')
         f.write('<head>\n')
         f.write('<style>\n')
@@ -158,7 +160,6 @@ content_frame = tk.Frame(root, padx=20, pady=20)
 content_frame.pack(fill=tk.BOTH, expand=True)
 
 account_history_path = ""
-history_path = ""
 
 def get_account_path(): # TODO add correct csv file check
     """Open csv file and store path in global variable"""
@@ -166,43 +167,35 @@ def get_account_path(): # TODO add correct csv file check
     account_history_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
     if account_history_path:
         acc_button.configure(bg='green', fg='white')
-        if history_path:
-            export_button.configure(bg='blue', fg='white')
+        export_button.configure(bg='blue', fg='white')
     else:
         acc_button.configure(bg='grey', fg='black')
         export_button.configure(bg='red', fg='white')
 
-def get_history_path():  # TODO add correct csv file check
-    """Open csv file and store path in global variable"""
-    global history_path
-    history_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-    if history_path:
-        history_button.configure(bg='green', fg='white')
-        if account_history_path:
-            export_button.configure(bg='blue', fg='white')
-    else:
-        history_button.configure(bg='grey', fg='black')
-        export_button.configure(bg='red', fg='white')
-
 def export():
     """Check if both csv files are selected, analyze data, and export html file"""
-    if account_history_path == "" or history_path == "":
+    if account_history_path == "":
         tk.messagebox.showerror("Error", "Please select both account history and history CSV files.")
     else:
-        data_frames = analyze_data(account_history_path, history_path)
-        export_html(data_frames[0], data_frames[1])
+        data_frames = analyze_data(account_history_path)
+        export_location = filedialog.asksaveasfilename(defaultextension=".html", filetypes=[("HTML Files", "*.html")])
+            
+        if export_location:
+            export_html(data_frames[0], data_frames[1], export_location)
+            tk.messagebox.showinfo("Success", "HTML file exported successfully.")
+        else:
+            tk.messagebox.showerror("Error", "Please select a valid export location.")
 
 
 acc_button = tk.Button(content_frame, text="Select 'Account History' CSV", command=get_account_path)
 acc_button.configure(bg='grey', fg='black', font=('Arial', 12), width=30)
 acc_button.pack()
 
-history_button = tk.Button(content_frame, text="Select 'History' CSV", command=get_history_path)
-history_button.configure(bg='grey', fg='black', font=('Arial', 12), width=30)
-history_button.pack()
-
 export_button = tk.Button(content_frame, text="Export HTML", command=export)
 export_button.configure(bg='red', fg='white', font=('Arial', 12), width=30)
 export_button.pack()
+
+version_label = tk.Label(root, text="Version Beta.1.0", font=('Arial', 10))
+version_label.pack(side=tk.RIGHT, anchor=tk.S)
 
 root.mainloop()
