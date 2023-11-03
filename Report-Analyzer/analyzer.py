d7122d991ce446b60adb6b5b32fc8e382f0543d3
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-11-02 20:57:13 -0400

    remove from main

--- Report-Analyzer/analyzer.py
+++ Report-Analyzer/analyzer.py
@@ -148,60 +148,55 @@ def export_html(details_df, total_df, export_location):
         f.write('</html>\n')
 
 
-def main():
-    # Create the Tkinter root
-    root = tk.Tk()
-    root.title("Report Analyzer")
-    root.geometry("300x300")
-    root.resizable(True, True)
-    root.config(background='cyan')
-    root.iconphoto(False, tk.PhotoImage(file=os.path.join(str(Path(__file__).resolve().parents[0]), 'ico.png')))
+# Create the Tkinter root
+root = tk.Tk()
+root.title("Report Analyzer")
+root.geometry("300x300")
+root.resizable(True, True)
+root.config(background='cyan')
+root.iconphoto(False, tk.PhotoImage(data="iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAADsAAAA7AF5KHG9AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAABcVJREFUWIXdl21MnWcZx3/X/ZzDAc44QAuFUmSlY2tpm9qkbkvUOPuhjWmaJnaBw6A0Nk02M1+21EwX4wtqzYyZJM4YLV9cgEN5idYtmYlrls1orC+0SdW4TgqWQsvcAU4Pp4cD5zzPffmhgHAGzZljMfFKrg/Pfd3X/f/dL9dzPw/8j03eV7aqNJzlE0b5NLAfqELZiDCplhsinLeGcwPNcmHdARp69OPG8rzfYUtlEanKIu4N+MgzAlZhPsP8RIJrbycIuZa3xPJM73EZXBeAcLc+K4ZTO8pwQwUUj0xzbTrJRmvZoEJclGLHMLmxkFtbN1IbmyV6dZJCC1/va5GfvC+Axi5tD/g4+pFqqt+aZHhqlmJVfugYzp1tlivAna2JsFuEowY+X15IbFsZNYNjRDPKmf4WOf1fATRF9KQRvv1wDZWD44ylXV63eXxhoFFur5XTHNFSV+ko8LNv3xZq/jDKpAdP9rXIL94TQGunbsoYrjxcQ+HlCcZn05zrb5Vnwt36NEr9XVIv9R2joynCmaJ89m8vo/LPN0im5rj/5ZOSMLkCpB2+Wn4P0WiSt1MZ/rlzmK8shJ5DuCyGi9muwj8QvoWIhpJ8bibFrdsZ4qX5xPLzOQWQE8AnX1cf0FpXRt3INEXG8KW2NrEAKCIwpcr3Ft0qR3pbpCNg6dKFVe54QjICp4YmcR7YxDajnMgZYNNNPprnkEilSahl6Gyz/GV53MvjZUe4b9E1j4bVxultld+6HnFrsY5D4LEe3ePLBUBgV3GA5DuzpNTwanbcmeeAFb6x9JyGpm5Npy2PrzLcq9Ekhwr9pGfm2ZkTgMLmgA8nlcZFGcuOewF+R4YnlrcZi2t8RD2bNRnheipNusCPzqSoygnAKD65s5mrVo1vjr3W8NmVSmSsy+nsTbaKLs5KBckJACE65+Hl+3AEqrPDLlw1wsCKFMX1lJlViGsKAvhvJUmj3MwJwBMuxecI1ldQMh7nIPDNFWIO96ErD55CxghXNGssIxwsL2TLeIy4wN9zAgjO88eUn5JgHkXG4YFwp+7uOy5/WxLzGBZn5QqoUiuG7wPeYltjt37MCKWOwXE90n2tcjmnMnzxhMwpdI9MMbS1hIQ6tLe16VKuMRwWKF3hwg7gsCjPAzx+Rv1Ae10Z7lCUEYQXIcf3wMIsn/tXgspNRVQWOtS9Wcd3AcTwNZQPq2XfcseSEeFEX6u0oyozQV4I5bEhFKBkOsUGH/wAILdDCDgOjyiYwTGiD9VQMzhGS7hLy/0eT3Udl+RaeQ39WuxE6Aj4eGhPFR+6cJ2owhcjx2QGcryMmiL6GVVOWzgo8GSBj0P7qql58x2Gp2cJobQ7hp/3tMjIYs5jPbrDWh5FeKo8SKyujNqLY0xkXH7W2ypti/3WBGg6q1vV41mEDMoRPA70HWeoMcKPBA47QsGOCmzQT9HwNNdjKcrVco/CFEKZA/HSIFPbNlAbTzE1NElA4Tu9LfLCcp1VARq6dLsRXqsKMRu9TUHG0ld/lS9fqeOnCjt9cCgj7DXKj/0Ooc0h5iuLqA348Hl652DNW9ITca5NzBB0Pa5Zw6mBFvlTtta7AMKduhvD+e3luNUlVKdd3AujRF3lBkrC5nFk6QNEVcIRDgBHRXgEuFctASApwqgKr1nh3ECz/GatlV4B0BDRvY7y6/oKMptDbFlsn8vg/n6UGRH2Z9+E71q9fnUGGsW7W5/ltlSG4W590FHO76rAWy4O4FrmFBBlzU+vRXsv4rBQhuFufVCEV3ZV4FYUsXl5h5l5EhfHSKvydM+x/5zy9TIJd+puMbyxpwpbFqR8eTA+R/ziOJ61nOxvlV+utziAEYdPlRQQzRaPpYgNjuNZaP2gxAGMzxKJzRG8HmN0sXF6ltilG6hCuL9FfvVBicNCFTRH9H4P3thehg34Cfz1JsYzPHq38llXAIBwp9aLw0tqKbQODXf7ofy/sn8Dpxhpdb4xu1QAAAAASUVORK5CYII="))
 
-    # Create a frame with padding
-    content_frame = tk.Frame(root, padx=20, pady=20)
-    content_frame.pack(fill=tk.BOTH, expand=True)
+# Create a frame with padding
+content_frame = tk.Frame(root, padx=20, pady=20)
+content_frame.pack(fill=tk.BOTH, expand=True)
 
-    account_history_path = ""
+account_history_path = ""
 
-    def get_account_path(): # TODO add correct csv file check
-        """Open csv file and store path in global variable"""
-        global account_history_path
-        account_history_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-        if account_history_path:
-            acc_button.configure(bg='green', fg='white')
-            export_button.configure(bg='blue', fg='white')
+def get_account_path(): # TODO add correct csv file check
+    """Open csv file and store path in global variable"""
+    global account_history_path
+    account_history_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+    if account_history_path:
+        acc_button.configure(bg='green', fg='white')
+        export_button.configure(bg='blue', fg='white')
+    else:
+        acc_button.configure(bg='grey', fg='black')
+        export_button.configure(bg='red', fg='white')
+
+def export():
+    """Check if both csv files are selected, analyze data, and export html file"""
+    if account_history_path == "":
+        tk.messagebox.showerror("Error", f"Please select the account history CSV file.")
+    else:
+        data_frames = analyze_data(account_history_path)
+        export_location = filedialog.asksaveasfilename(defaultextension=".html", filetypes=[("HTML Files", "*.html")])
+            
+        if export_location:
+            export_html(data_frames[0], data_frames[1], export_location)
+            tk.messagebox.showinfo("Success", "HTML file exported successfully.")
         else:
-            acc_button.configure(bg='grey', fg='black')
-            export_button.configure(bg='red', fg='white')
-
-    def export():
-        """Check if both csv files are selected, analyze data, and export html file"""
-        if account_history_path == "":
-            tk.messagebox.showerror("Error", "Please select the account history CSV file.")
-        else:
-            data_frames = analyze_data(account_history_path)
-            export_location = filedialog.asksaveasfilename(defaultextension=".html", filetypes=[("HTML Files", "*.html")])
-                
-            if export_location:
-                export_html(data_frames[0], data_frames[1], export_location)
-                tk.messagebox.showinfo("Success", "HTML file exported successfully.")
-            else:
-                tk.messagebox.showerror("Error", "Please select a valid export location.")
+            tk.messagebox.showerror("Error", "Please select a valid export location.")
 
 
-    acc_button = tk.Button(content_frame, text="Select 'Account History' CSV", command=get_account_path)
-    acc_button.configure(bg='grey', fg='black', font=('Arial', 12), width=30)
-    acc_button.pack()
+acc_button = tk.Button(content_frame, text="Select 'Account History' CSV", command=get_account_path)
+acc_button.configure(bg='grey', fg='black', font=('Arial', 12), width=30)
+acc_button.pack()
 
-    export_button = tk.Button(content_frame, text="Export HTML", command=export)
-    export_button.configure(bg='red', fg='white', font=('Arial', 12), width=30)
-    export_button.pack()
+export_button = tk.Button(content_frame, text="Export HTML", command=export)
+export_button.configure(bg='red', fg='white', font=('Arial', 12), width=30)
+export_button.pack()
 
-    version_label = tk.Label(root, text="Version Beta.1.0", font=('Arial', 10))
-    version_label.pack(side=tk.RIGHT, anchor=tk.S)
+version_label = tk.Label(root, text="Version Beta.1.0", font=('Arial', 10))
+version_label.pack(side=tk.RIGHT, anchor=tk.S)
 
-    root.mainloop()
-
-
-if __name__ == "__main__":
-    main()
+root.mainloop()
