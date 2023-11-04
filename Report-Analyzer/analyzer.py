e7d8757b21c3d254b1b3c4b944838b6ae476c77e
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-11-04 12:39:18 -0400

    Time frame reports added, html needs formatting

--- Report-Analyzer/analyzer.py
+++ Report-Analyzer/analyzer.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-# TODO Add monthly, and yearly summarys
+# TODO Format HTML better
 # TODO Check if TK operations work on Mac and Linux, if not using Qt6
 
 import_error = False
@@ -45,14 +45,14 @@ if import_error:
     sys.exit()
 
 
-def analyze_data(account_history_path):
+def analyze_data(account_history_path, time_frame):
     """Analyze the data from the CSV file and return the dataframe with the results"""
     # Read the CSV file
-    acc_df = pd.read_csv(account_history_path, sep=',')
+    account_df = pd.read_csv(account_history_path, sep=',')
 
     # remove whitespace from balance columns while keeping it a float
-    acc_df['Balance Before'] = acc_df['Balance Before'].str.replace('\xa0', '').astype(float)
-    acc_df['Balance After'] = acc_df['Balance After'].str.replace('\xa0', '').astype(float)
+    account_df['Balance Before'] = account_df['Balance Before'].str.replace('\xa0', '').astype(float)
+    account_df['Balance After'] = account_df['Balance After'].str.replace('\xa0', '').astype(float)
 
     # regex patterns to extract the data we need
     symbol_pattern = r"symbol (\w+:\w+)"
@@ -61,75 +61,87 @@ def analyze_data(account_history_path):
     position_type_pattern = r"Close (long|short) position"
 
     commission = 0.0
+    dataframes = {}
 
-    # Apply the regex patterns to each element of the 'Action' column
-    details_list = []
-    for _, row in acc_df.iterrows():
+    df_dict = {}
+    for _, row in account_df.iterrows():
+        # separate by time frame
+        time = ""
+        if time_frame == 1:  # daily
+            time = row['Time'].split(' ')[0]
+            if time not in df_dict:
+                df_dict[time] = {"details": []}
+        elif time_frame == 2:  # monthly
+            time = row['Time'].split(' ')[0].split('-')[0] + '-' + row['Time'].split(' ')[0].split('-')[1]
+            if time not in df_dict:
+                df_dict[time] = {"details": []}
+        elif time_frame == 3:  # quarterly
+            time = row['Time'].split(' ')[0].split('-')[0] + '-' + str(int(row['Time'].split(' ')[0].split('-')[1]) // 3)
+            if time not in df_dict:
+                df_dict[time] = {"details": []}
+        elif time_frame == 4:  # yearly
+            time = row['Time'].split(' ')[0].split('-')[0]
+            if time not in df_dict:
+                df_dict[time] = {"details": []}
+
         if "Commission" in row['Action']:
             commission += float(row['P&L'])
             continue
-        position = re.search(position_type_pattern, row['Action']).group(1)  # long or short position
-        symbol = re.search(symbol_pattern, row['Action']).group(1)  # symbol of the stock
-        quantity = re.search(shares_pattern, row['Action']).group(1)  # quantity of shares
-        opened_price = round(float(re.search(closed_price_pattern, row['Action']).group(1)) + ((row['Balance After'] - row['Balance Before']) / int(quantity)), 2)  # price at which the position was opened
-        closed_price = round(float(re.search(closed_price_pattern, row['Action']).group(1)), 2)  # price at which the position was closed
-        details_list.append({
+
+        df_dict[time]["details"].append({
             'Time': row['Time'],
-            'Position': position,
-            'Symbol': symbol,
-            'Quantity': quantity,
-            'Opened Price': opened_price,
-            'Closed Price': closed_price,
+            'Position': re.search(position_type_pattern, row['Action']).group(1),  # long or short position,
+            'Symbol': re.search(symbol_pattern, row['Action']).group(1),  # symbol of the stock,
+            'Quantity': re.search(shares_pattern, row['Action']).group(1),  # quantity of shares,
+            'Opened Price': round(float(re.search(closed_price_pattern, row['Action']).group(1)) + ((row['Balance After'] - row['Balance Before']) / int(re.search(shares_pattern, row['Action']).group(1))), 2),  # price at which the position was opened,
+            'Closed Price': round(float(re.search(closed_price_pattern, row['Action']).group(1)), 2),  # price at which the position was closed
             'Balance Before': round(row['Balance Before'], 2),
             'Balance After': round(row['Balance After'], 2),
             'P&L': round(row['Balance After'] - row['Balance Before'], 2),
             '%': round((row['Balance After'] - row['Balance Before']) / row['Balance Before'] * 100, 2),
         })
 
-    # Create a new DataFrame with only the columns we need
-    details_df = pd.DataFrame(details_list)
+    for time_frame in df_dict:
+        # Create a new DataFrame with only the columns we need
+        details_df = pd.DataFrame(df_dict[time_frame]["details"])
 
-    total_commission = round(commission, 2)
-    number_of_trades = [details_df['P&L'].count()]
-    number_of_long_trades = [details_df[details_df['Position'] == 'long']['P&L'].count()]
-    number_of_short_trades = [details_df[details_df['Position'] == 'short']['P&L'].count()]
-    net_profit_amount = [round(details_df['P&L'].sum() - total_commission, 2)]
-    total_profit_amount = [round(details_df[details_df['P&L'] > 0]['P&L'].sum(), 2)]
-    total_loss_amount = [round(details_df[details_df['P&L'] < 0]['P&L'].sum(), 2)]
-    total_return_percentage = [round(details_df['%'].sum(), 2)]
-    average_return_percentage = [round(details_df['%'].mean(), 2)]
-    batting_average = [round(details_df[details_df['P&L'] > 0]['P&L'].count() / details_df['P&L'].count() * 100, 2)]
-    average_win_percentage = [round(details_df[details_df['P&L'] > 0]['%'].mean(), 2)]
-    average_loss_percentage = [round(details_df[details_df['P&L'] < 0]['%'].mean(), 2)]
-    win_loss_ratio_percentage = [round(details_df[details_df['P&L'] > 0]['%'].mean() / abs(details_df[details_df['P&L'] < 0]['%'].mean()), 2)]
+        total_commission = round(commission, 2)
+        number_of_trades = [details_df['P&L'].count()]
+        number_of_long_trades = [details_df[details_df['Position'] == 'long']['P&L'].count()]
+        number_of_short_trades = [details_df[details_df['Position'] == 'short']['P&L'].count()]
+        net_profit_amount = [round(details_df['P&L'].sum() - total_commission, 2)]
+        total_profit_amount = [round(details_df[details_df['P&L'] > 0]['P&L'].sum(), 2)]
+        total_loss_amount = [round(details_df[details_df['P&L'] < 0]['P&L'].sum(), 2)]
+        total_return_percentage = [round(details_df['%'].sum(), 2)]
+        average_return_percentage = [round(details_df['%'].mean(), 2)]
+        batting_average = [round(details_df[details_df['P&L'] > 0]['P&L'].count() / details_df['P&L'].count() * 100, 2)]
+        average_win_percentage = [round(details_df[details_df['P&L'] > 0]['%'].mean(), 2)]
+        average_loss_percentage = [round(details_df[details_df['P&L'] < 0]['%'].mean(), 2)]
+        win_loss_ratio_percentage = [round(details_df[details_df['P&L'] > 0]['%'].mean() / abs(details_df[details_df['P&L'] < 0]['%'].mean()), 2)]
 
-    # Create a new DataFrame with only the columns we need for total values
-    total_df = pd.DataFrame({
-        'Number of Trades': [number_of_trades[0]],
-        'Number of Long Trades': [number_of_long_trades[0]],
-        'Number of Short Trades': [number_of_short_trades[0]],
-        'Total Return': [f"{total_return_percentage[0]}%"],
-        'Average Return': [f"{average_return_percentage[0]}%"],
-        'Batting Average': [f"{batting_average[0]}%"],
-        'Average Win': [f"{average_win_percentage[0]}%"],
-        'Average Loss': [f"{average_loss_percentage[0]}%"],
-        'Win Loss Ratio': [f"{win_loss_ratio_percentage[0]}%"],
-        'Commission': [f"${total_commission}"],
-        'Net Profit': [f"${net_profit_amount[0]}"],  # Commission appiled only to this
-        'Gross Profit': [f"${total_profit_amount[0]}"],
-        'Gross Loss': [f"${total_loss_amount[0]}"],
-    })
+        # Create a new DataFrame with only the columns we need for total values
+        total_df = pd.DataFrame({
+            'Number of Trades': [number_of_trades[0]],
+            'Number of Long Trades': [number_of_long_trades[0]],
+            'Number of Short Trades': [number_of_short_trades[0]],
+            'Total Return': [f"{total_return_percentage[0]}%"],
+            'Average Return': [f"{average_return_percentage[0]}%"],
+            'Batting Average': [f"{batting_average[0]}%"],
+            'Average Win': [f"{average_win_percentage[0]}%"],
+            'Average Loss': [f"{average_loss_percentage[0]}%"],
+            'Win Loss Ratio': [f"{win_loss_ratio_percentage[0]}%"],
+            'Commission': [f"${total_commission}"],
+            'Net Profit': [f"${net_profit_amount[0]}"],  # Commission appiled only to this
+            'Gross Profit': [f"${total_profit_amount[0]}"],
+            'Gross Loss': [f"${total_loss_amount[0]}"],
+        })
 
-    # Create the pie chart TODO make this a bar graphs of monthly data
-    # labels = ['Total Return', 'Average Return', 'Batting Average', 'Average Win', 'Average Loss', 'Win Loss Ratio']
-    # sizes = [total_return_percentage[0], average_return_percentage[0], batting_average[0], average_win_percentage[0], abs(average_loss_percentage[0]), win_loss_ratio_percentage[0]]
-    # plt.bar(labels, sizes, color=['green', 'green', 'green', 'green', 'red', 'green'])
-    # plt.savefig(os.path.join(str(Path(__file__).resolve()), 'output', 'pie_chart.png'))
+        dataframes[time_frame] = {"details": details_df, "total": total_df}
 
-    return details_df, total_df
+    return dataframes
 
 
-def export_html(details_df, total_df, export_location):
+def export_html(dataframes: dict, export_location):
     """Export the DataFrame to an HTML file"""
     if os.path.exists(export_location):
         os.remove(export_location)
@@ -156,11 +168,14 @@ def export_html(details_df, total_df, export_location):
         f.write('</style>\n')
         f.write('</head>\n')
         f.write('<body>\n')
-        f.write('<h2>Trade Details</h2>\n')
-        f.write(details_df.to_html(index=False, justify='center', border=1, bold_rows=True, na_rep=''))
-        f.write('<h2>Total Values</h2>\n')
-        f.write(total_df.to_html(index=False, justify='center', border=1, bold_rows=True, na_rep=''))
-        # f.write('<img src="pie_chart.png" alt="Pie Chart">\n')
+        for time_frame in dataframes:
+            details_df = dataframes[time_frame]["details"]
+            total_df = dataframes[time_frame]["total"]
+            f.write(f'<h1>{time_frame}</h1>\n')
+            f.write(total_df.to_html(index=False, justify='center', border=1, bold_rows=True, na_rep=''))
+            f.write('<h2>Trade Details</h2>\n')
+            f.write(details_df.to_html(index=False, justify='center', border=1, bold_rows=True, na_rep=''))
+            
         f.write('</body>\n')
         f.write('</html>\n')
 
@@ -196,6 +211,9 @@ file_button_frame.pack(fill=tk.BOTH, expand=True)
 version_frame = tk.Frame(container_frame)
 version_frame.pack(fill=tk.BOTH, expand=True)
 
+# Initialize values
+radio_var = tk.IntVar()
+radio_var.set(4)
 account_history_path = ""
 
 def get_account_path(): # TODO add correct csv file check
@@ -214,11 +232,11 @@ def export():
     if account_history_path == "":
         tk.messagebox.showerror("Error", f"Please select the account history CSV file.")
     else:
-        data_frames = analyze_data(account_history_path)
+        data_frames = analyze_data(account_history_path, radio_var.get())
         export_location = filedialog.asksaveasfilename(defaultextension=".html", filetypes=[("HTML Files", "*.html")])
             
         if export_location:
-            export_html(data_frames[0], data_frames[1], export_location)  # TODO Error handling
+            export_html(data_frames, export_location)  # TODO Error handling
             if tk.messagebox.askyesno("Success", "HTML file exported successfully. Do you want to open it?"):
                 webbrowser.open(export_location)
         else:
@@ -226,9 +244,6 @@ def export():
 
 
 # Create and label radio buttons
-radio_var = tk.IntVar()
-radio_var.set(4)
-
 report_title_label = tk.Label(radio_button_frame, text="Time Frame of Report")
 report_title_label.pack(side=tk.TOP, anchor=tk.N)
 report_title_label.config(font=('Arial', 12))
@@ -264,8 +279,6 @@ acc_button.bind("<Leave>", on_leave)
 
 export_button.bind("<Enter>", on_enter)
 export_button.bind("<Leave>", on_leave)
-
-
 
 # Create version label
 version_label = tk.Label(version_frame, text="Version Beta.1.1.1", font=('Arial', 10))
