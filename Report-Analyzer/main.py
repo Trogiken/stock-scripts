8aee52ed597bc204021d02900cad17b61ebdc4ad
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-11-07 16:41:03 -0500

    Url's updated and TODO list moved to project list

--- Report-Analyzer/main.py
+++ Report-Analyzer/main.py
@@ -4,14 +4,8 @@ import sys
 from source.gui import GUI
 
 current_version = "beta.2.3.3"
-# TODO Change url's once merged
-program_url = "https://github.com/Trogiken/stock-scripts/tree/Version-Checking/Report-Analyzer"
-version_url = "https://raw.githubusercontent.com/Trogiken/stock-scripts/Version-Checking/Report-Analyzer/version.txt"
-
-# TODO Work on commission calculations and usage
-# TODO Add export as PDF option
-# TODO Add option to export as Excel file
-# TODO Create graphs
+program_url = "https://github.com/Trogiken/stock-scripts/tree/master/Report-Analyzer"
+version_url = "https://raw.githubusercontent.com/Trogiken/stock-scripts/master/Report-Analyzer/version.txt"
 
 
 if __name__ == '__main__':
