1bc6056d2d3a9e2ccb345e0ee7f44dad2b96451e
Trogiken <70231827+Trogiken@users.noreply.github.com>
2023-11-07 16:36:13 -0500

    Timeout set to 3 seconds

--- Report-Analyzer/source/version_check.py
+++ Report-Analyzer/source/version_check.py
@@ -12,7 +12,7 @@ def get_latest(url: str):
     """Gets the latest version of the script from the GitHub repository."""
     version = None
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=3)
         version = response.text.strip()
     except Exception:
         pass
